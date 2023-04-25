adc739fecbf33a5bfa6035cade0b140ed7b6bfed
Pedro-Nepomuceno <pedrohnlima@gmail.com>
2023-04-24 18:34:55 -0700

    fix typo

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -17,7 +17,7 @@ const Hero = () => {
 						Hi, I'm <span className="text-[#915eff]">Pedro</span>{" "}
 					</h1>
 					<p className={`${styles.heroSubText} mt-2 text-white-100`}>
-						I'm full stack web developer
+						I'm a full stack web developer
 					</p>
 				</div>
 			</div>
