0fbaa135246e6ae4470a5339cf24a6489818a62d
Pedro-Nepomuceno <pedrohnlima@gmail.com>
2023-07-21 11:24:15 -0700

    update hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,13 @@ import { styles } from "../styles";
 // import { ComputersCanvas } from "./canvas";
 
 const Hero = () => {
-	return <section className="relative w-full h-screen mx-auto"></section>;
+	return (
+		<section className="relative w-full h-screen mx-auto">
+			<div className="z-5 min-h-screen-80 text-center flex flex-col justify-start items-center py-4 md:py-0 font-Grandslang sans-serif relative overflow-hidden">
+				<div className="min-h-screen-60 flex flex-col justify-center items-center mt-2.5vw mb-2.5vw relative"></div>
+			</div>
+		</section>
+	);
 };
 
 export default Hero;
