9fc89c32c9a1c8c16a9c1139fe4b4f65240f36cd
Pedro-Nepomuceno <pedrohnlima@gmail.com>
2023-07-23 11:05:25 -0700

    update text

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,16 +1,42 @@
 import React from "react";
 import { motion } from "framer-motion";
 import { styles } from "../styles";
-// import { ComputersCanvas } from "./canvas";
 
-const Hero = () => {
+const Hero = ({ text }) => {
+	const word = text.split("");
+	const container = {
+		hidden: { opacity: 0 },
+		visible: (i = 1) => ({
+			opacity: 1,
+			transition: { staggerChildren: 0.12, delayChildren: 0.04 * i },
+		}),
+	};
+
+	const child = {
+		visible: {
+			opacity: 1,
+			y: 0,
+			transition: { type: "spring", damping: 12, stiffness: 100 },
+		},
+		hidden: {
+			opacity: 0,
+			y: 20,
+			transition: { type: "spring", damping: 12, stiffness: 100 },
+		},
+	};
 	return (
-		<section className="flex items-center container  h-screen mx-auto">
-			<h1 className="block text-3xl">
+		<motion.div
+			variants={container}
+			initial="hidden"
+			animate="visible"
+			className="flex overflow-hidden items-center container  h-screen mx-auto">
+			<motion.h1 variants={child} className="block text-3xl">
 				CREATING ENGAGING USER INTERFACES AND SEAMLESS USER EXPERIENCES.
-				<p className="block py-4">FOR YOUR BUSINESS</p>
-			</h1>
-		</section>
+				<motion.p variants={child} className="block py-4">
+					FOR YOUR BUSINESS
+				</motion.p>
+			</motion.h1>
+		</motion.div>
 	);
 };
 
